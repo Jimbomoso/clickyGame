464688db0f01c71c20fb18431d1e56cd93698bf5
Jimbomoso <jimbomoso@gmail.com>
2019-08-08 20:55:54 -0400

    Updates

---
+++ precache-manifest.e60a436c8c8d04ba8ec3a23c1600d117.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc132964ed51b928c2b831d784b8765",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "0cc6309fc5a472587c83",
+    "url": "/clickygame/static/css/main.1b25b348.chunk.css"
+  },
+  {
+    "revision": "a93f9ccfb53ed61bff01",
+    "url": "/clickygame/static/js/2.52abbe24.chunk.js"
+  },
+  {
+    "revision": "0cc6309fc5a472587c83",
+    "url": "/clickygame/static/js/main.a580b01d.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "133cf1ce66049461451bb92a4318c4c6",
+    "url": "/clickygame/static/media/car1.133cf1ce.jpg"
+  },
+  {
+    "revision": "8f433a138481c1ab1a50e72eebea3803",
+    "url": "/clickygame/static/media/car10.8f433a13.jpg"
+  },
+  {
+    "revision": "c30f913f5084c2d52c13cb82be983896",
+    "url": "/clickygame/static/media/car11.c30f913f.jpg"
+  },
+  {
+    "revision": "dc2065d2fa3af06101392f4f45d57b1d",
+    "url": "/clickygame/static/media/car12.dc2065d2.jpg"
+  },
+  {
+    "revision": "52919fbe30b76f9c6e746b97fb7cd386",
+    "url": "/clickygame/static/media/car3.52919fbe.jpg"
+  },
+  {
+    "revision": "3246a082fd36e0fd90f53758ac90bc6c",
+    "url": "/clickygame/static/media/car4.3246a082.jpg"
+  },
+  {
+    "revision": "68d49b2dd47474bbc142d8ee686be76c",
+    "url": "/clickygame/static/media/car5.68d49b2d.jpg"
+  },
+  {
+    "revision": "df86231b8649a2506d14ab827b583cba",
+    "url": "/clickygame/static/media/car6.df86231b.jpg"
+  },
+  {
+    "revision": "ea88a52c7c6ba9298ac5e33bbabdc89c",
+    "url": "/clickygame/static/media/car7.ea88a52c.jpg"
+  },
+  {
+    "revision": "9b1dfb744bc4557eabde8418e145c8a2",
+    "url": "/clickygame/static/media/car8.9b1dfb74.jpg"
+  },
+  {
+    "revision": "2472c7f7c012f5c115162d6ed37ee8fe",
+    "url": "/clickygame/static/media/car9.2472c7f7.jpg"
+  }
+]);
